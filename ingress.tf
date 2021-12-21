9ecd2e4ecfeec51da46c617c8516144f2f64a4aa
Travis Saucier <travis.saucier@sourcefuse.com>
2021-12-21 11:03:41 -0500

    add health check module for ingress and service.

--- ingress.tf
+++ ingress.tf
@@ -1,7 +1,8 @@
+## 2048 game
+module "k8s_ingress_2048" {
+  source   = "./terraform-refarch-k8s-ingress"
 
-module "k8s_ingress" {
-  source = "./terraform-refarch-k8s-ingress"
-
+  default_name         = "ingress-2048"
   namespace            = "game-2048-2"
   default_service_type = "NodePort"
   enable_internal_alb  = false
@@ -18,7 +19,36 @@ module "k8s_ingress" {
 
   default_service_ports = [
     {
-      name        = "ingress-2048-2-port-80"
+      name        = "ingress-2048-port-80"
+      port        = 80
+      target_port = 80
+      protocol    = "TCP"
+    }
+  ]
+}
+
+## health checks
+module "k8s_ingress_health_check" {
+  source   = "./terraform-refarch-k8s-ingress"
+
+#  default_name         = "health-check-tss"
+  namespace            = "health-check"
+#  default_service_type = "ClusterIP"
+  enable_internal_alb  = false
+
+  default_service_selector = {
+    app = "nginx"
+  }
+
+  default_annotations = {
+    "kubernetes.io/ingress.class"           = "alb"
+    "alb.ingress.kubernetes.io/scheme"      = "internet-facing"
+    "alb.ingress.kubernetes.io/target-type" = "ip"
+  }
+
+  default_service_ports = [
+    {
+      name        = "health-check-port-80"
       port        = 80
       target_port = 80
       protocol    = "TCP"
