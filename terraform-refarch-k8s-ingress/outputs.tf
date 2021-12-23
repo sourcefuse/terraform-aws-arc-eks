165bc3b1d59499ecb4eda98a10bbdd8a4a9df9bc
Travis Saucier <travis.saucier@sourcefuse.com>
2021-12-23 11:24:01 -0500

    add better output

--- terraform-refarch-k8s-ingress/outputs.tf
+++ terraform-refarch-k8s-ingress/outputs.tf
@@ -2,6 +2,6 @@ output "default_service_name" {
   value = try(kubernetes_service.default.metadata.0.name, null)
 }
 
-output "default_ingress_status" {
-  value = kubernetes_ingress.default[0].status
+output "default_ingress_hostname" {
+  value = try(kubernetes_ingress.default[0].status[0]["load_balancer"][0]["ingress"][0]["hostname"], null)
 }
